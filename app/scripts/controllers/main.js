80d89dc9de8cae25c91adb577148661f740c54be
Guardabrazo <guardabrazo@MacBook-Pro-de-Federico.local>
2015-07-08 13:27:12 +0200

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dashboardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dashboardApp
+ */
+angular.module('dashboardApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    this.standardItems = [
+      { sizeX: 1, sizeY: 1, row: 0, col: 0 },
+      { sizeX: 2, sizeY: 2, row: 0, col: 2 },
+      { sizeX: 1, sizeY: 1, row: 0, col: 4 },
+      { sizeX: 1, sizeY: 1, row: 0, col: 5 },
+      { sizeX: 2, sizeY: 1, row: 1, col: 0 },
+      { sizeX: 1, sizeY: 1, row: 1, col: 4 },
+    ];
+  });
